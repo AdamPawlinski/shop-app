b8c3ac039a2fe7b272cc73ff5e992ccb97cfd4e1
Adam Pawliński <adam.pawlinski@gmail.com>
2019-03-21 19:55:31 +0100

    Updates

---
+++ precache-manifest.45e4b451cfa3c6a8de1b74cdfc457e03.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "8bf14641049dba83533e",
+    "url": "/shop-app/static/js/runtime~main.9593f1cb.js"
+  },
+  {
+    "revision": "5a79de95a9c13afca102",
+    "url": "/shop-app/static/js/main.bd4d1c65.chunk.js"
+  },
+  {
+    "revision": "b90596b62c536800f1e6",
+    "url": "/shop-app/static/js/2.a2666212.chunk.js"
+  },
+  {
+    "revision": "5a79de95a9c13afca102",
+    "url": "/shop-app/static/css/main.58fccf32.chunk.css"
+  },
+  {
+    "revision": "29a073584d279d89c5b4c61669b12eda",
+    "url": "/shop-app/index.html"
+  }
+];
